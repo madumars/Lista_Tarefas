922a9a54c8ffa5b3225a87d1964e4677bc707933
madumars <mariaeduardam08@gmail.com>
2026-03-06 13:23:46 -0300

    API dados

--- app/src/main/java/com/example/projetolistadetarefas/api/Service.java
+++ app/src/main/java/com/example/projetolistadetarefas/api/Service.java
@@ -1,11 +1,14 @@
 package com.example.projetolistadetarefas.api;
+
+import com.example.projetolistadetarefas.model.Post;
+import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public class Service {
 
-    interface ApiService {
-        @GET("/posts")
+    public interface ApiService {
+        @GET("posts")
         Call<List<Post>> getPosts();
-
+    }
 }

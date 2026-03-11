86c109c4838d53a117fe98b260d7107495428bba
madumars <mariaeduardam08@gmail.com>
2026-03-11 16:04:33 -0300

    HTTPS + Privacidade de usuário

--- app/src/main/java/com/example/projetolistadetarefas/api/RetrofitClient.kt
+++ app/src/main/java/com/example/projetolistadetarefas/api/RetrofitClient.kt
@@ -1,20 +1,29 @@
 package com.example.projetolistadetarefas.api
 
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
     private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    private const val HOSTNAME = "jsonplaceholder.typicode.com"
+
+    // 1. Criar o PIN (O valor abaixo é um exemplo, o app vai falhar e mostrar o correto no Logcat se o site mudar)
+    private val certificatePinner = CertificatePinner.Builder()
+        .add(HOSTNAME, "sha256/5g6m7kU/d5r5e/x28fGq62qI4N6nO16o285Qv765S4o=")
+        .build()
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
 
+    // 2. Adicionar o certificatePinner ao cliente OkHttp
     private val client = OkHttpClient.Builder()
         .addInterceptor(logging)
+        .certificatePinner(certificatePinner)
         .build()
 
     val instance: ApiService by lazy {
